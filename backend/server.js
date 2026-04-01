14f6e1729c28b2f3e14bb2a2b77a49203c3d93b5
Yeasin <yeasinmiazi1997@gmail.com>
2026-04-01 16:30:23 +0600

    vercel.json updated

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ const connectDB = require('./src/config/db');
 const { initCronJobs } = require('./src/jobs/scan.job');
 
 const PORT = process.env.PORT || 5000;
-
-// Connect to MongoDB, then start server
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
